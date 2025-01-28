e67e5a6685c47173506f98d163af559a1e12884f
SRV30 <142303159+SRV30@users.noreply.github.com>
2025-01-28 17:05:03 +0530

    first commit

---
+++ client/src/pages/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import DarkModeToggle from "../extras/DarkModeToggle";
+
+const Home = () => {
+  return (
+    <>
+      <DarkModeToggle />
+      <div className="">Home</div>
+    </>
+  );
+};
+
+export default Home;
