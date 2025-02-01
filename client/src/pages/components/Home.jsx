991949625a6d9f69592a62e32c14c7db2876d5bf
SRV30 <142303159+SRV30@users.noreply.github.com>
2025-02-01 14:41:09 +0530

    Updated index.html

--- client/src/pages/components/Home.jsx
+++ client/src/pages/components/Home.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import DarkModeToggle from "../extras/DarkModeToggle";
-import { CircularProgress } from "@mui/material";
 
 const Home = () => {
   return (
     <>
       <DarkModeToggle />
-      <div className="">Smart Care: Real Time Health Monitoring System</div>
-      <CircularProgress className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"/>
     </>
   );
 };
