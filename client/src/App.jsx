1dd097989c90ea18d2a080b944e9c7b42b3e2c6a
rajbhai129 <hemrajlalawat52@gmail.com>
2025-02-01 19:24:22 +0530

    Add ContactUs and About page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,11 @@ import Home from "./pages/components/Home";
 import { ToastContainer } from "react-toastify";
 import Footer from "./pages/components/Footer";
 import Products from "./pages/products/Products";
+import About from "./pages/components/About"
 import SingleProduct from "./pages/products/SingleProduct";
 import Review from "./pages/products/Review";
 import Cart from "./pages/orders/Cart";
+import ContactUs from "./pages/components/ContactUs";
 const App = () => {
   return (
     <div className="flex flex-col overflow-hidden bg-white dark:bg-black text-black dark:text-white">
@@ -19,6 +21,8 @@ const App = () => {
         <Route path="/singleproduct" element={<SingleProduct />} />
         <Route path="/singleproduct" element={<Review />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/ContactUs" element={<ContactUs />} />
       </Routes>
 
       <Footer />
