1a14f614a83f326ebe0e0c427de0f3af0d870344
Sahil Raj Verma <sahilrv196@gmail.com>
2025-02-02 13:50:47 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,11 +10,11 @@ import About from "./pages/components/About"
 import SingleProduct from "./pages/products/SingleProduct";
 import Review from "./pages/products/Review";
 import Cart from "./pages/orders/Cart";
-<<<<<<< HEAD
+
 import Wishlist from "./pages/orders/Wishlist";
 import ProductPostForm from "./pages/admin/ProductPostForm";
 import AdminDashboard from "./pages/admin/AdminDashboard";
-=======
+
 
 import MyOrders from "./pages/my-profile/MyOrders";
 import UpdatePassword from "./pages/my-profile/UpdatePassword";
@@ -24,7 +24,6 @@ import SavedAddress from "./pages/my-profile/SavedAddress";
 import ContactUs from "./pages/components/ContactUs";
 
 
->>>>>>> b67a2ee4ee9dca2b25b5a0a3a20884542986961b
 const App = () => {
   return (
     <div className="flex flex-col overflow-hidden bg-white dark:bg-black text-black dark:text-white">
@@ -45,15 +44,13 @@ const App = () => {
         <Route path="/singleproduct" element={<SingleProduct />} />
         <Route path="/singleproduct" element={<Review />} />
         <Route path="/cart" element={<Cart />} />
-<<<<<<< HEAD
         <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/productpostform" element={<ProductPostForm />} />
         <Route path="/AdminDashboard" element={<AdminDashboard/>} />
-=======
         <Route path="/About" element={<About />} />
         <Route path="/ContactUs" element={<ContactUs />} />
 
->>>>>>> b67a2ee4ee9dca2b25b5a0a3a20884542986961b
+
       </Routes>
 
       <Footer />
