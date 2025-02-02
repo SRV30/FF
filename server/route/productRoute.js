f6f3a4ce355820c96678409ec0d23ff4826d2e8a
ankitsunil530 <22bcs244@iiitdmj.ac.in>
2025-02-02 23:41:05 +0530

    Backend work

---
+++ server/route/productRoute.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { createProduct } from '../controllers/productController.js';
+
+const router = express.Router();
+
+router.route('/create').post(createProduct);
+
+
+export default router;
