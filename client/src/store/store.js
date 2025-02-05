9a7de19773965c94ec118708210dfc520c5a0e7c
ankitsunil530 <22bcs244@iiitdmj.ac.in>
2025-02-06 00:26:52 +0530

    Backend work in Progress

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import darkModeReducer from "./extra/darkModeSlice";
 import authReducer from "./auth-slice/user";
-
+import productReducer from "./auth-slice/productSlice";
+import addressReducer from "./address-slice/addressSlice";
 const store = configureStore({
   reducer: {
     darkMode: darkModeReducer,
     auth: authReducer,
+    product: productReducer,
+    address: addressReducer,
   },
 });
 
